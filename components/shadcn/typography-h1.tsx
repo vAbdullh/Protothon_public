730650af36e00e6bd87fa41a85a39abfd423ed2b
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-07-31 18:37:12 +0300

    chore: added shadcn components

---
+++ components/shadcn/typography-h1.tsx
@@ -0,0 +1,7 @@
+export function TypographyH1({text}) {
+  return (
+    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+      {text || "not set"}
+    </h1>
+  )
+}
---
+++ components/shadcn/typography-h3.tsx
@@ -0,0 +1,7 @@
+export function TypographyH3({text}) {
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+     {text || "not set"}
+    </h3>
+  )
+}
