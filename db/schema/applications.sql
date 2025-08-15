754d28c3d98fae645b3d9626f806710afea32a48
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-08-15 07:53:47 +0300

    feat(db): update schema, create table to track activities, and create a dummy data seed

--- db/schema/applications.sql
+++ db/schema/applications.sql
@@ -6,5 +6,8 @@ CREATE TABLE applications (
   idea_description TEXT NOT NULL CHECK (char_length(idea_description) <= 250),
   leader_id UUID NOT NULL REFERENCES members(id),
   status TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('pending', 'approved', 'rejected')),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_by UUID,
+  action_note TEXT CHECK (char_length(action_note) <= 100)
 );
