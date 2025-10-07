64460be15e7aab28e47d37582b61cdc276892ca7
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-10-07 18:58:52 +0300

    Make admin panel home is the auth page

--- app/[locale]/auth/page.js
+++ app/[locale]/auth/page.js
@@ -1,18 +1,17 @@
 'use client'
-import Link from "next/link";
 import { useTranslations } from "next-intl";
 import UIControls from "@/components/ui-controls";
 import { H1 } from "@/components/shadcn/typography-h1";
 import { LoginForm } from "@/components/login-form";
-import { UserRoundPen } from "lucide-react";
 
-export default function Home() {
+export default function page() {
   const t = useTranslations('shared');
 
   return (
     <main className="flex flex-col gap-[32px] items-center justify-center h-screen px-5">
-      <div className="flex gap-1 justify-center items-center" >
-        <UserRoundPen className="size-12"/>
+      <div className="flex flex-col gap-1 justify-center items-center" >
+        {/* icon */}
+        <img src="/protothon-logo-purple.png" alt="Protothon Logo" className="w-32 h-auto mx-auto" />
         <H1>{t('protothon')}</H1>
       </div>
       <LoginForm />
--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,56 +1,8 @@
-'use client'
+import React from 'react'
+import AuthPage from './auth/page'
 
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { useTranslations } from "next-intl";
-import UIControls from "@/components/ui-controls";
-
-export default function Home() {
-  const t = useTranslations('shared');
-  const [status, setStatus] = useState("");
-
-  useEffect(() => {
-    const getHealth = async () => {
-      try {
-        const res = await fetch("/api/health");
-        const data = await res.json();
-        setStatus(data.status); // or whatever your API returns
-      } catch (error) {
-        setStatus("Failed to fetch health");
-      }
-    };
-
-    getHealth();
-  }, []);
-
+export default function page() {
   return (
-    <div className="font-sans grid place-items-center h-screen">
-      <main className="flex flex-col gap-[32px] items-center">
-        <Image
-          src="./next.svg"
-          alt="Protothon Logo"
-          width={150}
-          height={150}
-          className="mb-8 dark:invert"
-        />
-        <h1 className="text-4xl font-bold">{t('protothon')}</h1>
-        <p className="text-lg text-gray-500">
-          Under development... 🛠️
-        </p>
-        <p className="text-lg">
-          API Health:&nbsp;
-          {status ? (
-            <span className="text-green-600">{status}</span>
-          ) : (
-            <span className="text-red-600">Error</span>
-          )}
-        </p>
-        <Link href="/dashboard" className="text-blue-600 hover:text-blue-800 underline">
-          Go to Dashboard
-        </Link>
-        <UIControls />
-      </main>
-    </div>
-  );
+    <AuthPage />
+  )
 }
