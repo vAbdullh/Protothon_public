86d22352512d2bfe3e3ff0bb5c4a4ac5cd5ea067
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-12-03 20:43:16 +0300

    Update discord link

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -452,7 +452,7 @@ function Community() {
 
       {/* Button */}
       <a
-        href="https://discord.gg/ehF8tjbtWS"
+        href="https://discord.gg/8x7JNpExcX"
         target="_blank"
         rel="noopener noreferrer"
         className="w-1/2 lg:w-1/4"
